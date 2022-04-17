95c02e527c240c91de37041d3abb9c9ee9a83d45
別府大地 <beppudaichi@beppudaichis-MacBook-Air.local>
2022-04-17 11:56:42 +0900

    値オブジェクトにcontentTypeを追加した

--- entity/Content.ts
+++ entity/Content.ts
@@ -5,36 +5,42 @@ class Page {
     public readonly contentId: string;
     public readonly hostPageId: string;
     public readonly text: string;
+    public readonly contentType: string;
 
     constructor(
         contentId: string,
         hostPageId: string,
         text: string,
+        contentType: string
     ) {
         this.contentId = contentId
         this.hostPageId = hostPageId;
         this.text = text;
+        this.contentType = contentType;
     }
 
     public static fromJSON = (json: any) => {
         console.log('josn', json)
-        const { contentId, hostPageId, text } = Object.assign({}, json);
+        const { contentId, hostPageId, text, contentType } = Object.assign({}, json);
 
         return new Page(
             contentId,
             hostPageId,
             text,
+            contentType,
         );
     }
 
     public static createContent(
         hostPageId:string,
         text: string,
+        contentType: string
     ) {
         return new Page(
             uuidv4(),
             hostPageId,
             text,
+            contentType
         )
     }
 
