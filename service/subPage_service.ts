7bfaea3dfa4f9fcc53347f4ba3b5970851aec323
別府大地 <beppudaichi@beppudaichis-MacBook-Air.local>
2022-04-19 09:22:55 +0900

    fetch処理を追加(hostIdが一致のデータ全部取得)

--- service/subPage_service.ts
+++ service/subPage_service.ts
@@ -1,6 +1,24 @@
+import axios from "axios";
+import { Dispatch, SetStateAction } from "react";
+
 import SubPage from "../entity/SubPage";
 
 export async function createSubPage(subPage: SubPage) {
     //postでsubPageのデータを渡す
     console.log('createSubPage', subPage)
+}
+
+export async function fecthSubPages(setSubPages: Dispatch<SetStateAction<SubPage[]>>, hostPageId: string) {
+    //postでhostPageIdを渡す
+    await axios
+    .get('http://localhost:3001/read_subPages')
+    .then((results) => {
+        console.log(results.data.pages);
+        console.log('originallyPagesMap',results.data.pages.map((doc: any) => SubPage.fromJSON(doc.data)))
+        setSubPages(results.data.pages.map((doc: any) => SubPage.fromJSON(doc)))
+    })
+    .catch((error) => {
+        console.log('read_subPagesのやろうが通信失敗');
+        console.log(error.status);
+    });
 }
