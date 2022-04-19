b5250ab06db67b3396baf8c020318bda157c4e8a
別府大地 <beppudaichi@beppudaichis-MacBook-Air.local>
2022-04-19 09:19:04 +0900

    subPageを作成できるようになった

--- component/page/page_list_tile.tsx
+++ component/page/page_list_tile.tsx
@@ -1,9 +1,11 @@
-import { Button, IconButton } from "@material-ui/core"
+import { Button, IconButton, TextField } from "@material-ui/core"
 import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
 import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
 import { useState } from "react";
 
 import Page from "../../entity/Page"
+import SubPage from "../../entity/SubPage";
+import { createSubPage } from "../../service/subPage_service";
 
 type Props = {
     page: Page
@@ -11,9 +13,11 @@ type Props = {
 
 export default function PageListTile(props: Props) {
     const [isConfirmation, setIsConfimation] = useState(false)
+    const [pageName, setPageName] = useState('')
 
     const addSubPage = async () => {
-
+        const subPageData = SubPage.createSubPage(props.page.pageId, pageName, '') 
+        await createSubPage(subPageData)
     }
 
     const openIsConfirmationField = () => {
@@ -34,12 +38,20 @@ export default function PageListTile(props: Props) {
         </div>
         {/*  */}
         <div>{isConfirmation ?
-            <div className='flex flex-col fixed top-40 left-60 justify-center z-50 w-56 h-32 bg-white'>
+            <div className='flex flex-col fixed top-40 left-60 justify-center z-50 w-56 h-48 bg-white px-8 py-4'>
+                <TextField
+                    variant='outlined'
+                    style={{ backgroundColor: 'white' }}
+                    label='ページ名'
+                    onChange={(e) => setPageName(e.target.value)}
+                />
+                <div className='my-2'>{!(pageName == '') && 
                 <Button
                     onClick={addSubPage}
                 >
                     サブページ作成
                 </Button>
+                }</div>
                 <IconButton onClick={closeIsConfirmationField} className='flex items-center'>
                     <CancelOutlinedIcon/>
                 </IconButton>
---
+++ service/subPage_service.ts
@@ -0,0 +1,6 @@
+import SubPage from "../entity/SubPage";
+
+export async function createSubPage(subPage: SubPage) {
+    //postでsubPageのデータを渡す
+    console.log('createSubPage', subPage)
+}
